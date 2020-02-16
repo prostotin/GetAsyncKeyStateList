15171b72ce89858855b59c51dcf56d88a38fa514
Oleksandr Prostotin <37674127+prostotin@users.noreply.github.com>
2020-02-15 21:24:25 -0500

    Update keys.cpp

--- keys.cpp
+++ keys.cpp
@@ -1,6 +1,3 @@
-// allKeys.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <Windows.h>
 #include <fstream>
@@ -8,8 +5,7 @@ int main()
 {
 	std::cout << "press keys(chars) to generate GetAsyncKeyState statements..." << std::endl;
 	char c;
-	std::ofstream myfile("GetAsyncKeyState.txt");
-	
+	std::ofstream myfile("GetAsyncKeyState.txt");	
 	for (;;) {	
 		std::cin >> c;
 		myfile << "else if (GetAsyncKeyState('" << char(toupper(c)) << "') & 0x8000) {} //if key " << c << " is pressed.\n" << std::endl;
