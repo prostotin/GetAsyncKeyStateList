ef25c96927d1115dda726eae8048f58f7b117bc8
Oleksandr Prostotin <37674127+prostotin@users.noreply.github.com>
2020-02-15 21:08:42 -0500

    Create keys.cpp
    Generates a file with all chars pressed. Go 1 by 1 and press enter, it will generate a list of pressed keys. Alternatively copy paste it from the .txt files provided.

---
+++ keys.cpp
@@ -0,0 +1,23 @@
+// allKeys.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+int main()
+{
+	std::cout << "press keys(chars) to generate GetAsyncKeyState statements..." << std::endl;
+	char c;
+	std::ofstream myfile("GetAsyncKeyState.txt");
+	
+	for (;;) {	
+		std::cin >> c;
+		myfile << "else if (GetAsyncKeyState(" << c << ") & 0x8000) {} //if key " << c << " is pressed.\n" << std::endl;
+		if (c == 'm') {
+			return 0;
+		}
+	}
+	myfile.close();
+	return 0;
+}
+
